08b2974d94c6b079d6dfb8bb416c8b07f0828a7d
Alejandro <alemg77@yahoo.com.ar>
2020-05-18 21:23:03 -0300

    Snack bar para undo delete.

--- app/src/main/java/com/example/entregableandroid/Vista/FragmentListaItems/FragmentListaItems.java
+++ app/src/main/java/com/example/entregableandroid/Vista/FragmentListaItems/FragmentListaItems.java
@@ -1,10 +1,12 @@
 package com.example.entregableandroid.Vista.FragmentListaItems;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,12 +17,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.entregableandroid.MainActivity;
 import com.example.entregableandroid.Modelo.ApiML.ItemListaAPI;
 import com.example.entregableandroid.Modelo.ApiML.ResultadoBusquedaAPI;
 import com.example.entregableandroid.R;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Collections;
 import java.util.List;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
@@ -30,6 +36,7 @@ public class FragmentListaItems extends Fragment implements ProductoAdapter.Prod
     private FragmentListaItems.Aviso listener;
     private List<ItemListaAPI> listaElementos;
     private ProductoAdapter productoAdapter;
+    private ItemListaAPI elementoBorrado;
 
     public FragmentListaItems(){
     }
@@ -79,20 +86,39 @@ public class FragmentListaItems extends Fragment implements ProductoAdapter.Prod
 
         @Override
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-            int posicion = viewHolder.getAdapterPosition();
+            final int posicion = viewHolder.getAdapterPosition();
 
             switch (direction){
                 case ItemTouchHelper.LEFT:  // <----
+
+                    elementoBorrado = listaElementos.get(posicion);
                     listaElementos.remove(posicion);
                     productoAdapter.notifyItemRemoved(posicion);
+                    Snackbar.make(recyclerViewProducto, "Regresar?", Snackbar.LENGTH_LONG)
+                            .setAction("Si", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    listaElementos.add(posicion, elementoBorrado);
+                                    productoAdapter.notifyItemInserted(posicion);
+                                }
+                            }).show();
                     break;
 
                 case ItemTouchHelper.RIGHT: // ---->
                     break;
             }
-
-
             Log.d(TAG,"Se desplazo elemento");
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(R.color.colorPrimaryDark)
+                    .addBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimary))
+                    .addActionIcon(R.drawable.ic_delete_sweep_black_24dp)
+                    .create()
+                    .decorate();
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
         }
     };
 
